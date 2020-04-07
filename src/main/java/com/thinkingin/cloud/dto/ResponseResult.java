c66ac71bbaf603eedf16b7f9179a48a197d042c9
silverlight7 <284400743@qq.com>
2020-04-07 10:17:58 +0800

    init

--- src/main/java/com/thinkingin/cloud/dto/ResponseResult.java
+++ src/main/java/com/thinkingin/cloud/dto/ResponseResult.java
@@ -14,6 +14,7 @@ import lombok.ToString;
 @ToString(callSuper = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseResult<T> {
+
     private boolean success;
     private String message;
     private T result;
